8e4fe2a3dcd47112770391a4c770f051cebb91c9
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2022-10-28 20:29:55 -0300

    (test) new sequelize initialization method

--- src/db.js
+++ src/db.js
@@ -6,25 +6,25 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT
 } = process.env;
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`, {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  });
 
-const sequelize = new Sequelize({
-  database: DB_DATABASE,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl:{
-      require:true,
-      rejectUnauthorized: false
-    }
-  }
-})
+// const sequelize = new Sequelize({
+//   database: DB_DATABASE,
+//   username: DB_USER,
+//   password: DB_PASSWORD,
+//   host: DB_HOST,
+//   port: DB_PORT,
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl:{
+//       require:true,
+//       rejectUnauthorized: false
+//     }
+//   }
+// })
 
 const basename = path.basename(__filename);
 
